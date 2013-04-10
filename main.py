f5bc60e20d77735a2675f805851a6c134c7f82e2
wh <wh_linux@126.com>
2013-04-10 13:55:59 +0800

    fix

--- main.py
+++ main.py
@@ -6,10 +6,9 @@
 #   Date    :   13/04/02 15:31:15
 #   Desc    :   程序接口
 #
+from config import QQ, QQ_PWD
 from webqq.webqq import WebQQ
 
 if __name__ == "__main__":
-    QQ = 1685359365
-    QQ_PWD = "4esz$ESZ"
     webqq = WebQQ(QQ, QQ_PWD)
     webqq.run()
